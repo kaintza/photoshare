7bcf8cd0a8b4a4fecd3e4a6d149ea3ad146eed74
turkimurki <adam.kaintz@gmail.com>
2015-07-30 17:07:45 +0200

    posts add to index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
 
     def create
       @post = Post.create(post_params)
-        redirect_to posts_path
+      redirect_to posts_path
     end
 
     private
