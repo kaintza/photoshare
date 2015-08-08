98bab1ae87dfbb4f9ab6e8bd46cdba1d5711fa2d
turkimurki <adam.kaintz@gmail.com>
2015-08-08 20:15:39 +0200

    refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,53 +2,53 @@ class PostsController < ApplicationController
 
 before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    def index
-      @posts = Post.all
-    end
+  def index
+    @posts = Post.all
+  end
 
-    def new
-      @post = Post.new
-    end
+  def new
+    @post = Post.new
+  end
 
-    def create
-      if @post = Post.create(post_params)
-        flash[:success] = "Your post has been created"
-        redirect_to posts_path
-      else
-        flash.now[:alert] = "Your new post couldn't be created! Please check the form."
-        render :new
-      end
+  def create
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Please check the form."
+      render :new
     end
+  end
 
-    def show
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Update failed! Please check the form."
+      render :edit
     end
+  end
 
-    def edit
-    end
+  def destroy
+    @post.destroy
+    redirect_to(posts_path)
+  end
 
-    def update
-      if @post.update(post_params)
-        flash[:success] = "Post updated"
-        redirect_to posts_path
-      else
-        flash.now[:alert] = "Update failed! Please check the form."
-        render :edit
-      end
-    end
+  private
 
-    def destroy
-      @post.destroy
-      redirect_to(posts_path)
-    end
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 
-    private
-
-    def post_params
-      params.require(:post).permit(:image, :caption)
-    end
-
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
 end
